78cec3fe5d6324588861d03aea97186a940c7188
tannerla <leontanner@gmail.com>
2019-04-11 21:10:00 +0100

    refresh wording and linkage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from "react";
+// import React from "react";
 // import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 
-const App = () => <div>Blurb to go here</div>;
+// const App = () => <div>Blurb to go here</div>;
 
 // ReactDOM.render(<App />, document.getElementById("root"));
 
